3efd145b179d869f0c959853db7a5cef089faaea
Justin Oeni <joeni@andrew.cmu.edu>
2024-11-12 16:09:39 -0500

    Initial Commit

---
+++ EmojiHunter/EmojiHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiHunterApp.swift
+//  EmojiHunter
+//
+//  Created by Justin Oeni on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiHunterTests/EmojiHunterTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiHunterTests.swift
+//  EmojiHunterTests
+//
+//  Created by Justin Oeni on 12/11/24.
+//
+
+import Testing
+@testable import EmojiHunter
+
+struct EmojiHunterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
